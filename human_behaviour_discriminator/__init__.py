7739a7c09a671ae5cb601fdd195c64b22c2db61d
lp21110 <lp21110@bristol.ac.uk>
2026-07-06 15:01:32 +0100

    updated code

--- human_behaviour_discriminator/__init__.py
+++ human_behaviour_discriminator/__init__.py
@@ -7,11 +7,7 @@ from .get_behaviour_analysis import get_behaviour_analysis
 def get_behavioural_analysis_interactive(rubric=BEHAVIOUR_CATEGORY_RUBRIC):
     user_input_text_reply = ask_for_text_input()
     user_dimensions_REPLY = ask_for_behaviour_dimensions(rubric)
-    analysis_output = get_behaviour_analysis(
-        text_input=user_input_text_reply,
-        rubric=rubric,
-        user_dimension_reply=user_dimensions_REPLY,
-    )
+    analysis_output = get_behaviour_analysis(text_input=user_input_text_reply, rubric=rubric, user_dimension_reply=user_dimensions_REPLY,)
     return analysis_output 
 
 #result_interactive_input = get_behavioural_analysis_interactive(rubric=BEHAVIOUR_CATEGORY_RUBRIC)
